6de34e0f560ac8ecce67d85baa952f33c4795f68
dzemil5 <dzemil.sahovic@gmail.com>
2024-10-03 15:05:37 +0200

    Dodata Login logika

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div>
-      App Works!
-    </div>
-  );
-}
+import React, { useContext } from 'react';
+import './App.css';
+
+
+import { AuthContext } from './components/Login/AuthContext';
+import QuoteList from './components/Quotes/QuoteList';
+import Login from './components/Login/Login';
+
+const App = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  if (isLoggedIn) {
+    return (
+      <div>
+        <h1>Quotes</h1>
+        <QuoteList />
+      </div>
+    );
+  } else {
+    return <Login />;
+  }
+};
 
 export default App;
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { AuthContext } from './AuthContext';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  useEffect(() => {
+    const storedLoginStatus = localStorage.getItem('isLoggedIn');
+    if (storedLoginStatus === 'true') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:8000/sessions', { username, password })
+      .then((response) => {
+        if (response.status === 200) {
+          setIsLoggedIn(true);
+        } else {
+          setError('Invalid username or password');
+        }
+      })
+      .catch((error) => {
+        setError('Error logging in');
+      });
+  };
+
+  const handleLogout = () => {
+    localStorage.setItem('isLoggedIn', 'false');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className='login'>
+      <h1>Login</h1>
+      {isLoggedIn ? (
+        <div>
+          <p>Welcome, {username}!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Login;
